9369055f98da2fdbbee2679b02bac71f82863798
Combatd <combatdstar@gmail.com>
2021-03-28 14:53:10 -0700

    Inject props from Provider BirdStore

--- src/Components/Birds/index.js
+++ src/Components/Birds/index.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
 
-export default class Birds extends Component {
+@inject("BirdStore")
+@observer
+class Birds extends Component {
   render() {
+    // access our store via the props
+    const { BirdStore } = this.props;
+
     return <div>Birds</div>;
   }
 }
+
+export default Birds;
