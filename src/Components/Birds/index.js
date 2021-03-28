23b9755df2c41296abe2384e2bc86f456a1e0759
Combatd <combatdstar@gmail.com>
2021-03-28 15:00:06 -0700

    Finish Birds Component

--- src/Components/Birds/index.js
+++ src/Components/Birds/index.js
@@ -4,11 +4,34 @@ import { inject, observer } from "mobx-react";
 @inject("BirdStore")
 @observer
 class Birds extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.BirdStore.addBird(this.birdInput.value);
+    e.target.reset();
+  };
+
   render() {
-    // access our store via the props
     const { BirdStore } = this.props;
 
-    return <div>Birds</div>;
+    return (
+      <div>
+        <h2>You have {BirdStore.birdCount} birds</h2>
+
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            ref={input => (this.birdInput = input)}
+            placeholder="Add a bird"
+          />
+        </form>
+
+        <ul>
+          {BirdStore.birds.map(bird => (
+            <li key={bird}>{bird}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
